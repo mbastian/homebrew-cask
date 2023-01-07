ae3577c65c876acc32106e81f2ba9092eebe396b
Mathieu Bastian <mathieu.bastian@gmail.com>
2023-01-07 22:31:48 +0100

    Update gephi.rb to 0.10.0

--- Casks/gephi.rb
+++ Casks/gephi.rb
@@ -1,8 +1,13 @@
 cask "gephi" do
-  version "0.9.7"
-  sha256 "82bd1206b777011cf15bd4ff4b1ade7e4ffa09d1262c1c890b238c23626e4861"
+  arch = Hardware::CPU.intel? ? "x64" : "aarch64"
+  version "0.10.0"
+  if Hardware::CPU.intel?
+    sha256 "8bb3af6e2a32a3745edc342209d90438365227b40dd477e05522c497b2e2bccf"
+  else
+    sha256 "0e8f0a61eb98c93a608ada5068f8d9a5981981a28b087c0b2e1b9356145895bf"
+  end
 
-  url "https://github.com/gephi/gephi/releases/download/v#{version}/gephi-#{version}-macos-x64.dmg",
+  url "https://github.com/gephi/gephi/releases/download/v#{version}/gephi-#{version}-macos-#{arch}.dmg",
       verified: "github.com/gephi/gephi/"
   name "Gephi"
   desc "Open-source platform for visualizing and manipulating large graphs"
